7f9ade2410ab3c4304d34e4ea4d04136b32b770d
Elian Vantomme <elian.vantomme@hotmail.be>
2021-12-04 11:37:25 +0100

    HEEE HEEE

--- express-bandbase/public/javascripts/bandsInTownApi.js
+++ express-bandbase/public/javascripts/bandsInTownApi.js
@@ -1,10 +1,37 @@
-fetch("https://rest.bandsintown.com/artists/Sabaton/events?app_id=070843b5d6760df0f070c233e2c0a688", {
+function searchArtist(){
+    const artist = document.getElementById("artistInput").value.trim();
+    const url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=070843b5d6760df0f070c233e2c0a688`;
+    document.getElementById('bandsInTown').value="Fetching ...";
+    fetch(url,{"method": "GET",})
+        .then(response => response.json())
+        .then( data =>{
+            let output = ''
+            data.forEach(function (object){
+                output +=`
+                    <ul>
+                       <li>${object.lineup}</li>
+                    </ul>  
+            `
+                })
+            document.getElementById("bandsInTown").innerHTML = output;
+            })
+
+
+}
+
+
+
+
+
+
+fetch("", {
     "method": "GET",
 
 })
     .then(response => response.json())
     .then(data => {
         let output = '';
+
         data.forEach(function (object){
             output +=`
             <ul>
@@ -12,8 +39,8 @@ fetch("https://rest.bandsintown.com/artists/Sabaton/events?app_id=070843b5d6760d
             </ul>           
 
 
-`
+        `;
         })
         document.getElementById('bandsInTown').innerHTML = output;
         console.log(output);
-    })
+    });
