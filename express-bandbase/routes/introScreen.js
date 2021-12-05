0a68cad0424bbe4d8b4676ebfe487e00f6c0efbb
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2021-12-05 13:26:59 +0100

    ImageNamer

--- express-bandbase/routes/introScreen.js
+++ express-bandbase/routes/introScreen.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fs = require('fs');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+//global.document = new JSDOM(pug).window.document;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('introScreen', {title: 'Intro Screen - Band Base' });
 });
 
-//AJAX
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', "/randomBandImages/1240.jpg");
-ourRequest.onload = function() {
 
-};
+
+
+
 
 module.exports = router;
